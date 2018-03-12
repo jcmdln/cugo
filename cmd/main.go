4264eac799b42ba94dbfe922b8cfe8f729e132cf
jcmdln <jcmdln@gmail.com>
2018-03-12 00:36:31 -0400

    main.go: Cleanup output

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -13,9 +12,9 @@ var RootCmd = &cobra.Command{
 	Long:  "cugo is a multi-call binary that provides core utilities for Unix systems.",
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) < 1 {
-			fmt.Println("cugo: No operands passed\n" +
+			fmt.Printf("cugo: No operands passed\n" +
 				"Usage: cugo [COMMAND] [OPTIONS] ARGUMENTS ...\n")
-			os.Exit(0)
+			return
 		}
 	},
 }
