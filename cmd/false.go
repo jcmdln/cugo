e972e0c4ba20f9df9ed5cfbeef0107c35af7e596
jcmdln <jcmdln@gmail.com>
2018-05-07 09:00:59 -0400

    Add true.go; Add false.go

---
+++ cmd/false.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	falseCmd = &cobra.Command{
+		Use:   "false",
+		Short: "return false value",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			False()
+		},
+	}
+)
+
+func init() {
+	RootCmd.AddCommand(falseCmd)
+}
+
+func False() bool {
+	return false
+}
---
+++ cmd/true.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var (
+	trueCmd = &cobra.Command{
+		Use:   "true",
+		Short: "return true value",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			True()
+		},
+	}
+)
+
+func init() {
+	RootCmd.AddCommand(trueCmd)
+}
+
+func True() bool {
+	return true
+}
