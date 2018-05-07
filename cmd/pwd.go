75a32e7b9b0d3a933d93bd504b6d6641a0c501f1
jcmdln <jcmdln@gmail.com>
2018-05-06 23:07:30 -0400

    pwd.go: partial implementation

---
+++ cmd/pwd.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	pwdCmd = &cobra.Command{
+		Use:   "pwd",
+		Short: "return working directory name",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			Pwd()
+		},
+	}
+
+	pwdL bool
+	pwdP bool
+)
+
+func init() {
+	RootCmd.AddCommand(pwdCmd)
+}
+
+func Pwd() {
+	p, _ := os.Getwd()
+	fmt.Printf("%s\n", p)
+
+	return
+}
