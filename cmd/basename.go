91a7a85e4786c6addd9112a4c3420f723278351f
jcmdln <jcmdln@gmail.com>
2018-05-06 21:59:01 -0400

    basename.go: initial commit

---
+++ cmd/basename.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	basenameCmd = &cobra.Command{
+		Use:   "basename",
+		Short: "return non-directory portion of a pathname",
+		Long:  "",
+		Run: func(cmd *cobra.Command, args []string) {
+			Basename(args)
+		},
+	}
+)
+
+func init() {
+	RootCmd.AddCommand(basenameCmd)
+}
+
+func Basename(args []string) {
+	b := filepath.Base(args[0])
+	fmt.Printf("%s\n", b)
+
+	return
+}
