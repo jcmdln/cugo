aaa5c9a03da4bdf007df41ecb34f7cb3687b38ac
jcmdln <jcmdln@gmail.com>
2018-05-13 21:09:30 -0400

    basename.go: catch error when < 1 argument is passed

--- cmd/basename.go
+++ cmd/basename.go
@@ -13,6 +13,12 @@ var (
 		Short: "return non-directory portion of a pathname",
 		Long:  "",
 		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) < 1 {
+				fmt.Printf("cugo: basename: No arguments passed\n" +
+					"Usage: basename ARGUMENTS...\n")
+				return
+			}
+
 			Basename(args)
 		},
 	}
