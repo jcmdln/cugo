913856977bf1663c880c6ff5eb8cf0b66c74a292
jcmdln <jcmdln@gmail.com>
2018-05-15 23:55:26 -0400

    src/touch: Relocate inline functions

--- src/touch/touch.go
+++ src/touch/touch.go
@@ -13,27 +13,27 @@ var (
 	Verbose bool
 )
 
+func exists(t string) bool {
+	_, err := os.Stat(t)
+	if os.IsNotExist(err) {
+		return false
+	} else {
+		return true
+	}
+}
+
+func verbose(t string) {
+	if Verbose {
+		fmt.Printf("cugo: touch: Created %s\n", t)
+	}
+}
+
 func Touch(args []string) {
-	Exists := func(t string) bool {
-		_, err := os.Stat(t)
-		if os.IsNotExist(err) {
-			return false
-		} else {
-			return true
-		}
-	}
-
-	Verbose := func(t string) {
-		if Verbose {
-			fmt.Printf("cugo: touch: Created %s\n", t)
-		}
-	}
-
 	for _, target := range args {
-		if Exists(target) == false {
+		if exists(target) == false {
 			if !Create {
 				os.Create(target)
-				Verbose(target)
+				verbose(target)
 			}
 		}
 	}
