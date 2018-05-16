5c5d3540a454c01183a3c2adc89cd46a645710d1
jcmdln <jcmdln@gmail.com>
2018-05-15 23:52:18 -0400

    src/mkdir.go: Relocate inline functions

--- src/mkdir/mkdir.go
+++ src/mkdir/mkdir.go
@@ -13,38 +13,38 @@ var (
 	Verbose bool
 )
 
+func exists(t string) bool {
+	_, err := os.Stat(t)
+	if os.IsNotExist(err) {
+		return false
+	} else {
+		return true
+	}
+}
+
+func verbose(t string) {
+	if Verbose {
+		fmt.Printf("cugo: mkdir: Created %s\n", t)
+	}
+}
+
 func Mkdir(args []string) {
-	Exists := func(t string) bool {
-		_, err := os.Stat(t)
-		if os.IsNotExist(err) {
-			return false
-		} else {
-			return true
-		}
-	}
-
-	Verbose := func(t string) {
-		if Verbose {
-			fmt.Printf("cugo: mkdir: Created %s\n", t)
-		}
-	}
-
 	for _, target := range args {
 		if Parents {
 			c := "."
 			t := strings.Split(filepath.Clean(target), "/")
 			for i := range t {
 				c += "/" + t[i]
-				if Exists(c) == false {
+				if exists(c) == false {
 					os.Mkdir(c, os.FileMode(Mode))
-					Verbose(c)
+					verbose(c)
 				}
 			}
 		} else {
-			if Exists(filepath.Dir(target)) == true {
+			if exists(filepath.Dir(target)) == true {
 				os.Mkdir(target,
 					os.FileMode(Mode))
-				Verbose(target)
+				verbose(target)
 			} else {
 				fmt.Printf("cugo: mkdir: '%s' doesn't exist: "+
 					"missing parent directory.\n", filepath.Dir(target))
