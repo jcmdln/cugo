f82c6b7c02f7f04a1847cf4c29448c43b7cfad4a
jcmdln <jcmdln@gmail.com>
2018-09-21 14:08:14 -0400

    Add boilerplate for chmod

---
+++ cmd/chmod.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"github.com/jcmdln/cugo/src/chmod"
+	"github.com/spf13/cobra"
+)
+
+var (
+	chmodCmd = &cobra.Command{
+		Use:   "chmod",
+		Short: "Change file mode bits",
+		Long:  "",
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			chmod.Chmod(args)
+		},
+	}
+)
+
+func init() {
+	RootCmd.AddCommand(chmodCmd)
+	chmodCmd.Flags().SortFlags = false
+	chmodCmd.Flags().BoolVarP(&chmod.Changes, "changes", "c", false,
+		"Report only when changes are made")
+	chmodCmd.Flags().BoolVarP(&chmod.Quiet, "quiet", "q", false,
+		"Suppress most error messages")
+	chmodCmd.Flags().BoolVarP(&chmod.Verbose, "verbose", "v", false,
+		"Report each processed file")
+	chmodCmd.Flags().BoolVarP(&chmod.Recursive, "recursive", "r", false,
+		"Change files and directories recursively")
+}
---
+++ src/chmod/chmod.go
@@ -0,0 +1,12 @@
+package chmod
+
+import ()
+
+var (
+	Changes   bool
+	Quiet     bool
+	Verbose   bool
+	Recursive bool
+)
+
+func Chmod(args []string) {}
