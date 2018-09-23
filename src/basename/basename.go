62708b04eddb107ffbbd3c6afe00ab71f3afbe10
jcmdln <jcmdln@gmail.com>
2018-09-22 22:40:52 -0400

    Adding an inline variable for clarity (I guess)

--- src/basename/basename.go
+++ src/basename/basename.go
@@ -5,9 +5,6 @@ import (
 	"path/filepath"
 )
 
-// basename takes any number of args and uses filepath.Base to return the
-// last element of a path. To conform to POSIX behavior, it will only act
-// on the first provided arg.
 func Basename(args []string) {
 	arg := args[0]
 	fmt.Printf("%s\n", filepath.Base(arg))
