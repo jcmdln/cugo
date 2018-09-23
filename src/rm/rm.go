af764874d4761468b7482afb112895eb2b939c2e
jcmdln <jcmdln@gmail.com>
2018-09-22 23:33:12 -0400

    rm: Fix logic error when removing single file

--- src/rm/rm.go
+++ src/rm/rm.go
@@ -17,19 +17,30 @@ var (
 	Verbose     bool
 )
 
-func remove(t string) {
-	if Force {
-		os.Remove(t)
-		if Verbose {
-			fmt.Printf("cugo: rm: Removed '%s'\n", t)
+// if Force {
+// 	os.Remove(t)
+// 	if Verbose {
+// 		fmt.Printf("cugo: rm: Removed '%s'\n", t)
+// 	}
+// }
+
+func rm(target string) {
+	err := os.Remove(target)
+	if err != nil {
+		fmt.Printf("cugo: %s\n", err)
+	}
+	if Verbose {
+		fmt.Printf("cugo: rm: Removed '%s'\n", target)
+	}
+}
+
+func remove(target string) {
+	if !Force && Interactive {
+		if p.Prompt("Remove '" + target + "'?") {
+			rm(target)
 		}
-	} else if Interactive {
-		if p.Prompt("Remove '" + t + "'?") {
-			os.Remove(t)
-			if Verbose {
-				fmt.Printf("cugo: rm: Removed '%s'\n", t)
-			}
-		}
+	} else {
+		rm(target)
 	}
 }
 
@@ -37,13 +48,13 @@ func Rm(args []string) {
 	for _, target := range args {
 		t, err := os.Stat(target)
 		if os.IsNotExist(err) {
-			fmt.Printf("cugo: rm %s: no such file or directory", target)
+			fmt.Printf("cugo: rm %s: no such file or directory\n", target)
 			return
 		}
 
 		if t.IsDir() {
 			if Dir && e.Empty(target) {
-				remove(target)
+				rm(target)
 				return
 			}
 
@@ -51,23 +62,23 @@ func Rm(args []string) {
 				for !e.Empty(target) {
 					filepath.Walk(target, func(t string, info os.FileInfo, err error) error {
 						if info.IsDir() && e.Empty(t) {
-							remove(t)
+							rm(t)
 						}
 						if !info.IsDir() {
-							remove(t)
+							rm(t)
 						}
 						return nil
 					})
 				}
 
 				if e.Empty(target) {
-					remove(target)
+					rm(target)
 				}
 			} else {
 				fmt.Printf("cugo: rm %s: is a directory", target)
 			}
 		} else {
-			remove(target)
+			rm(target)
 		}
 	}
 }
