1718ce3b3a4c388ece76c53bb262ee75aa10b3d6
jcmdln <jcmdln@gmail.com>
2018-09-23 23:38:22 -0400

    sleep: allocate outside of loop

--- src/sleep/sleep.go
+++ src/sleep/sleep.go
@@ -3,18 +3,28 @@ package sleep
 import (
 	"strings"
 	"time"
+
+	er "github.com/jcmdln/cugo/lib/error"
 )
 
 func Sleep(args string) {
 	a := strings.Split(args, " ")
 
+	var (
+		s   []string
+		t   time.Duration
+		err error
+	)
+
 	for _, i := range a {
-		s := strings.Split(i, "")
+		s = strings.Split(i, "")
 		if len(s) < 2 {
 			i += "s"
 		}
 
-		t, _ := time.ParseDuration(i)
-		time.Sleep(t)
+		t, err = time.ParseDuration(i)
+		if !er.Error("cugo", err) {
+			time.Sleep(t)
+		}
 	}
 }
