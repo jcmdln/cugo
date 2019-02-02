53280547bf7bc4c6f189c3b4bf03f8f51903d3a1
jcmdln <jcmdln@gmail.com>
2019-02-01 22:06:59 -0500

    yes: remove io import, it's not much faster

--- src/yes/yes.go
+++ src/yes/yes.go
@@ -9,20 +9,19 @@
 package yes
 
 import (
-	"io"
-	"os"
+	"fmt"
 	"strings"
 )
 
 func Yes(args []string) {
 	if len(args) == 0 {
-		for true {
-			io.WriteString(os.Stdout, "y\n")
+		for {
+			fmt.Printf("y\n")
 		}
 	} else {
-		for true {
-			out := strings.Join(args, " ") + "\n"
-			io.WriteString(os.Stdout, out)
+		out := strings.Join(args, " ")
+		for {
+			fmt.Printf("%s\n", out)
 		}
 	}
 }
