e3f9b31515ec5c1b3e257a26e5dc30db9dc147ad
jcmdln <jcmdln@gmail.com>
2019-02-07 17:26:48 -0500

    touch: fix frontend

--- cmd/touch.go
+++ cmd/touch.go
@@ -16,29 +16,30 @@ type touchCmd struct {
 	description string
 
 	help bool
+	touch.Options
 }
 
 func (u *touchCmd) Prepare(flags *flagger.Flags) {
 	u.name, u.usage = "touch", "[-acm] [-d ccyy-mm-ddTHH:MM:SS[.frac][Z]] [-r FILE] FILE ..."
 	u.description = "Change file access and modification times"
 
-	flags.BoolVar(&touch.Access, "Change the access time", "-a")
-	flags.BoolVar(&touch.Create, "Do not create missing files", "-c")
-	flags.StringVar(&touch.Date, "", "Change access and modified time as RFC3339Nano", "-d")
-	flags.BoolVar(&touch.Modified, "Change the modified time", "-m")
-	flags.StringVar(&touch.Reference, "", "Use access and modified time of file", "-r")
+	flags.BoolVar(&u.Access, "Change the access time", "-a")
+	flags.BoolVar(&u.Create, "Do not create missing files", "-c")
+	flags.StringVar(&u.Date, "", "Change access and modified time as RFC3339Nano", "-d")
+	flags.BoolVar(&u.Modified, "Change the modified time", "-m")
+	flags.StringVar(&u.Reference, "", "Use access and modified time of file", "-r")
 	flags.BoolVar(&u.help, "Show help output", "-h", "--help")
 }
 
 func (u *touchCmd) Action(s []string, flags *flagger.Flags) error {
 	if data, err := flags.Parse(s); err != nil {
-		touch.Touch(data)
+		u.Touch(data)
 	} else {
 		if u.help {
 			help.Help(u.name, u.usage, u.description, flags)
 		}
 
-		touch.Touch(data)
+		u.Touch(data)
 	}
 
 	return nil
