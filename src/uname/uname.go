4b239144db807a1bf05d5a657befc8fdffc3f58d
jcmdln <jcmdln@gmail.com>
2019-05-19 07:29:19 -0400

    uname: reduce []byte to non-null characters

--- src/uname/uname.go
+++ src/uname/uname.go
@@ -29,25 +29,25 @@ func (opt *Options) Uname() error {
 			opt.Sysname = true
 			opt.Nodename = true
 			opt.Release = true
-			opt.Version = true
+			//opt.Version = true
 			opt.Machine = true
 		}
 	}
 
 	if opt.Sysname {
-		out = append(out, string(uname.Sysname[:]))
+		out = append(out, strings.Trim(string(uname.Sysname[:]), "\x00"))
 	}
 	if opt.Nodename {
-		out = append(out, string(uname.Nodename[:]))
+		out = append(out, strings.Trim(string(uname.Nodename[:]), "\x00"))
 	}
 	if opt.Release {
-		out = append(out, string(uname.Release[:]))
+		out = append(out, strings.Trim(string(uname.Release[:]), "\x00"))
 	}
 	if opt.Version {
-		out = append(out, string(uname.Version[:]))
+		out = append(out, strings.Trim(string(uname.Version[:]), "\x00"))
 	}
 	if opt.Machine {
-		out = append(out, string(uname.Machine[:]))
+		out = append(out, strings.Trim(string(uname.Machine[:]), "\x00"))
 	}
 
 	if _, err = fmt.Printf("%s\n", strings.Join(out, " ")); err != nil {
