d165da635a8865d3d51d814d89df13c30778e5bd
jcmdln <jcmdln@desktop.local>
2020-02-22 20:48:07 -0500

    Naively respect terminal width when printing available commands

--- cugo.go
+++ cugo.go
@@ -15,27 +15,47 @@ import (
 	"sort"
 
 	"github.com/jcmdln/cugo/cmd"
+	"github.com/jcmdln/cugo/lib/term"
 )
 
 func main() {
-	if err := cmd.Command.Parse(os.Args[1:]); err != nil {
-		var (
-			s, c string
-			l    []string
-			i    int
-		)
+	var (
+		command     string
+		commandList []string
+		currentLine string
+		index       int
+		termWidth   int
 
-		l = cmd.Command.List()
-		sort.Strings(l)
+		err error
+	)
 
-		for i, c = range l {
-			s += c
-			if i != len(l)-1 {
-				s += " "
+	if err = cmd.Command.Parse(os.Args[1:]); err != nil {
+		fmt.Printf("error: %s\n", err)
+
+		if termWidth, _, err = term.Size(int(os.Stdin.Fd())); err != nil {
+			fmt.Print(err)
+			os.Exit(1)
+		}
+
+		commandList = cmd.Command.List()
+		sort.Strings(commandList)
+
+		fmt.Printf("\nAvailable commands:\n")
+
+		for index, command = range commandList {
+			if len(currentLine)+len(command) >= termWidth {
+				fmt.Printf("%s\n", currentLine)
+				currentLine = ""
+			}
+
+			currentLine += command
+			if index != len(commandList)-1 {
+				currentLine += " "
 			}
 		}
 
-		fmt.Printf("%s\n\nAvailable commands:\n%s\n", err, s)
+		fmt.Printf("%s\n", currentLine)
+
 		os.Exit(1)
 	}
 
