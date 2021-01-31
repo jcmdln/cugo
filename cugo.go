0f3403443c3cbd229dbd8c949655b747b5938425
jcmdln <jcmdln@gmail.com>
2021-01-31 18:04:24 -0500

    Use full in-file license header

--- cugo.go
+++ cugo.go
@@ -1,6 +1,20 @@
-// Copyright 2018 Johnathan C Maudlin
-// Use of this source code is governed by an MIT-style license that
-// may be found in the LICENSE file.
+// cugo.go
+//
+// Copyright 2021 Johnathan C. Maudlin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
 
 // Unix utilities as a multi-call binary
 //
@@ -20,13 +34,12 @@ import (
 
 func main() {
 	var (
+		err         error
 		command     string
 		commandList []string
 		currentLine string
 		index       int
 		termWidth   int
-
-		err error
 	)
 
 	if err = cmd.Command.Parse(os.Args[1:]); err != nil {
