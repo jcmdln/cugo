ab2e1c2a2e5ce9b8ae5bb65ae4cde1145974d42d
jcmdln <jcmdln@gmail.com>
2022-04-22 10:04:56 -0400

    Switch to ISC License; Bump copyright date

--- src/uname/uname.go
+++ src/uname/uname.go
@@ -1,20 +1,20 @@
 // uname.go
 //
-// Copyright 2021 Johnathan C. Maudlin
+// ISC License
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software.
+// Copyright (c) 2022 Johnathan C. Maudlin <jcmdln@gmail.com>
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-// IN THE SOFTWARE.
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+// OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
 
 package uname
 
@@ -36,16 +36,16 @@ func (opt *Options) Uname() error {
 		return err
 	}
 
-    if !opt.Nodename && !opt.Release && !opt.Version && !opt.Machine {
-        opt.Sysname = true
+	if !opt.Nodename && !opt.Release && !opt.Version && !opt.Machine {
+		opt.Sysname = true
 	}
 
-    if opt.All {
-        opt.Nodename = true
-        opt.Release = true
-        opt.Version = true
-        opt.Machine = true
-    }
+	if opt.All {
+		opt.Nodename = true
+		opt.Release = true
+		opt.Version = true
+		opt.Machine = true
+	}
 
 	if opt.Sysname {
 		out = append(out, strings.Trim(string(uname.Sysname[:]), "\x00"))
